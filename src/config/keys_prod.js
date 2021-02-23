2d9c94322aa52d8bfc32862da366b1317b6e9ffb
Immortal <immortal@localhost.localdomain>
2021-02-24 00:05:47 +0530

    second commit

--- src/config/keys_prod.js
+++ src/config/keys_prod.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGODB_URI,
+  mongoURI:
+    "mongodb+srv://Nitesh:EvHlH9ld1VedJi79@cluster0.s98hn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   secretOrKey: process.env.SECRET_OR_KEY,
 };
